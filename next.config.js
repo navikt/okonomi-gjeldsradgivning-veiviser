1b9a5c8a93f2d120f84371dded9d82b332c9b8d1
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-06-26 12:00:56 +0200

    Set up next.js

---
+++ next.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const withLess = require("@zeit/next-less");
+const packageJson = require("./package.json");
+const constants = require("./src/constants.json");
+const navFrontendModuler = [];
+Object.keys(packageJson.dependencies).forEach((key) => {
+    if (key.startsWith("nav-frontend-")) {
+        navFrontendModuler.push(key);
+    }
+});
+const withTranspileModules = require("next-transpile-modules")(navFrontendModuler);
+const isProd = process.env.NODE_ENV === "production";
+module.exports = withTranspileModules(
+    withLess({
+        target: "server",
+        exportTrailingSlash: false,
+        assetPrefix: isProd ? constants.CONTEXT_PATH : "",
+        webpack: (config, options) => {
+            config.module.rules.push({
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: "@svgr/webpack",
+                        options: {
+                            svgoConfig: {
+                                plugins: [
+                                    {
+                                        removeTitle: false,
+                                    },
+                                ],
+                            },
+                        },
+                    },
+                ],
+            });
+            config.resolve.modules.push(path.resolve("./src"));
+            return config;
+        },
+    })
+);
