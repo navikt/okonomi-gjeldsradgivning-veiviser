436fcca7df4665af641a1ca1eeb74482883b83df
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-07-14 22:56:28 +0200

    Bytter til port 8080 for express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 const contextPath = "/okonomi-og-gjeldsradgivning";
 const staticDir = __dirname + "/out";
 
