52eb675654d13b02c231b50d80514100fba787c2
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-08-25 14:55:48 +0200

    Legger til :active i sidebar for artikkelen som vises

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,30 +4,69 @@ import Lenke from 'nav-frontend-lenker';
 
 import { SanityArticleGroup, SanityArticle, SanityLink } from '../sanityDocumentTypes';
 import { PanelIcon } from './PanelIcon';
+import { useEffect, useState } from 'react';
 
-export const Sidebar = (props: { articleGroup: SanityArticleGroup }) => (
-    <aside className="sidebar">
-        <Panel className="sidebar-panel">
-            <PanelIcon imageUrl={props.articleGroup.iconUrl} />
+const getArticleOffsets = (articles: SanityArticle[]) => {
+    return articles
+        .map((article) => document.getElementById(article.slug))
+        .map((node) => ({
+            offset: node?.offsetTop ?? 0,
+            id: node.id,
+        }));
+};
 
-            <Ingress>{props.articleGroup.title}</Ingress>
+const getFirstIdAfterCurrentOffset = (offset: number, nodes: { offset: number; id: string }[]): string => {
+    if (offset < 100) {
+        return '';
+    }
+    const ids = nodes.filter((node) => offset < node.offset + 30);
+    if (ids && ids.length > 0) {
+        return ids.shift().id;
+    }
+    return '';
+};
 
-            {props.articleGroup.articles?.map((article: SanityArticle) => (
-                <Normaltekst key={article.slug} className="sidebar-link__link">
-                    <Lenke href={`#${article.slug}`}>{article.title}</Lenke>
-                </Normaltekst>
-            ))}
+export const Sidebar = (props: { articleGroup: SanityArticleGroup }) => {
+    const [currentArticle, setCurrentArticle] = useState('');
 
-            <div className="divider" />
+    useEffect(() => {
+        window.onscroll = () => {
+            const articleOffsets = getArticleOffsets(props.articleGroup.articles ?? []);
+            const currentOffset = window.pageYOffset;
 
-            {props.articleGroup.links?.map((link: SanityLink) => (
-                <Normaltekst key={link.title} className="sidebar-link__link">
-                    <Lenke href={link.href}>
-                        <img src={link.iconUrl} />
-                        <span>{link.title}</span>
-                    </Lenke>
-                </Normaltekst>
-            ))}
-        </Panel>
-    </aside>
-);
+            if (articleOffsets) {
+                const nextArticle = getFirstIdAfterCurrentOffset(currentOffset, articleOffsets);
+                setCurrentArticle(nextArticle);
+            }
+        };
+    }, []);
+
+    return (
+        <aside className="sidebar">
+            <Panel className="sidebar-panel">
+                <PanelIcon imageUrl={props.articleGroup.iconUrl} />
+
+                <Ingress>{props.articleGroup.title}</Ingress>
+
+                {props.articleGroup.articles?.map((article: SanityArticle) => (
+                    <Normaltekst key={article.slug} className="sidebar-link__link">
+                        <Lenke className={currentArticle === article.slug && 'lenke--active'} href={`#${article.slug}`}>
+                            {article.title}
+                        </Lenke>
+                    </Normaltekst>
+                ))}
+
+                <div className="divider" />
+
+                {props.articleGroup.links?.map((link: SanityLink) => (
+                    <Normaltekst key={link.title} className="sidebar-link__link">
+                        <Lenke href={link.href}>
+                            <img src={link.iconUrl} />
+                            <span>{link.title}</span>
+                        </Lenke>
+                    </Normaltekst>
+                ))}
+            </Panel>
+        </aside>
+    );
+};
