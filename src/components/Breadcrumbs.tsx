697578786de525e0568f98e659597f182e0b9722
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-08-26 13:03:35 +0200

    Gjør brødsmulestien mer responsiv

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,12 +1,25 @@
 import Link from 'next/link';
+import NavFrontendChevron from 'nav-frontend-chevron';
+import { Normaltekst } from 'nav-frontend-typografi';
+import MediaQuery from 'react-responsive';
 
 export const Breadcrumbs = (props: { title: string }) => {
     return (
         <nav className="breadcrumbs">
-            <Link href="/">
-                <a className="lenke">Økonomi- og gjeldsrådgivning</a>
-            </Link>
-            / {props.title}
+            <MediaQuery minWidth={310}>
+                <Link href="/">
+                    <a className="breadcrumbs__previous lenke">Økonomi- og gjeldsrådgivning</a>
+                </Link>
+                <NavFrontendChevron type="høyre" />
+                <div className="breadcrumbs__current">
+                    <Normaltekst>{props.title}</Normaltekst>
+                </div>
+            </MediaQuery>
+            <MediaQuery maxWidth={309}>
+                <Link href="/">
+                    <a className="lenke">Til forsiden</a>
+                </Link>
+            </MediaQuery>
         </nav>
     );
 };
