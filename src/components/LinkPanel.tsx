caf3ae208128ec94a0322a23c07bec4cba4aeb2d
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-09-02 13:12:12 +0200

    Justerer høyde på header dynamisk og legger til støtte for ikon på lenkepanel

--- src/components/LinkPanel.tsx
+++ src/components/LinkPanel.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { LenkepanelBase } from 'nav-frontend-lenkepanel';
 import { Innholdstittel } from 'nav-frontend-typografi';
+import MediaQuery from 'react-responsive';
 
 import { SanityBlockContent } from './SanityBlockContent';
 import { SanityLinkPanel } from '../sanityDocumentTypes';
@@ -11,6 +12,14 @@ export const LinkPanel = (props: { linkPanel: SanityLinkPanel }) => (
         linkCreator={(linkProps) => (
             <Link href="/articles/[slug]" as={linkProps.href}>
                 <div className={`section-panel section-panel__noIcon lenkepanel`} style={{ cursor: 'pointer' }}>
+                    {props.linkPanel.iconUrl && (
+                        <MediaQuery minWidth={376}>
+                            <div className="section-panel__image">
+                                <img className="section-panel__icon" alt="" src={props.linkPanel.iconUrl} />
+                            </div>
+                        </MediaQuery>
+                    )}
+
                     <div>
                         <Innholdstittel>{props.linkPanel.title}</Innholdstittel>
                         <SanityBlockContent blocks={props.linkPanel.description} />
