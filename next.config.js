4640c2ec6efbd7455469914b53b9f5833826667b
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-09-08 08:22:54 +0200

    Fjerner assetPrefix slik at assets loades riktig

--- next.config.js
+++ next.config.js
@@ -9,33 +9,10 @@ Object.keys(packageJson.dependencies).forEach((key) => {
     }
 });
 const withTranspileModules = require('next-transpile-modules')(navFrontendModuler);
-const isProd = process.env.NODE_ENV === 'production';
 module.exports = withTranspileModules(
     withLess({
         basePath: '/okonomi-og-gjeld',
         target: 'server',
         trailingSlash: false,
-        assetPrefix: isProd ? constants.CONTEXT_PATH : '',
-        webpack: (config, options) => {
-            config.module.rules.push({
-                test: /\.svg$/,
-                use: [
-                    {
-                        loader: '@svgr/webpack',
-                        options: {
-                            svgoConfig: {
-                                plugins: [
-                                    {
-                                        removeTitle: false,
-                                    },
-                                ],
-                            },
-                        },
-                    },
-                ],
-            });
-            config.resolve.modules.push(path.resolve('./src'));
-            return config;
-        },
     })
 );
