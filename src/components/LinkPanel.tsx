61d422ad1de255a21fd92240117f15589940c5ad
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-09-11 12:31:57 +0200

    Riktig styling på lenkepanel

--- src/components/LinkPanel.tsx
+++ src/components/LinkPanel.tsx
@@ -10,20 +10,21 @@ export const LinkPanel = (props: { linkPanel: SanityLinkPanel }) => (
         href={`/articles/${props.linkPanel.slug}`}
         linkCreator={(linkProps) => (
             <Link href="/articles/[slug]" as={linkProps.href}>
-                <a>
-                    <div className={`section-panel section-panel__noIcon lenkepanel`} style={{ cursor: 'pointer' }}>
-                        {props.linkPanel.iconUrl && (
-                            <div className="section-panel__image">
-                                <img className="section-panel__icon" alt="" src={props.linkPanel.iconUrl} />
-                            </div>
-                        )}
-
-                        <div>
-                            <Innholdstittel>{props.linkPanel.title}</Innholdstittel>
-                            <SanityBlockContent blocks={props.linkPanel.description} />
+                <a
+                    className="lenkepanel lenkepanel-border section-panel section-panel__noIcon"
+                    style={{ cursor: 'pointer' }}
+                >
+                    {props.linkPanel.iconUrl && (
+                        <div className="section-panel__image">
+                            <img className="section-panel__icon" alt="" src={props.linkPanel.iconUrl} />
                         </div>
-                        <span className="lenkepanel__indikator" />
+                    )}
+
+                    <div>
+                        <Innholdstittel>{props.linkPanel.title}</Innholdstittel>
+                        <SanityBlockContent blocks={props.linkPanel.description} />
                     </div>
+                    <span className="lenkepanel__indikator" />
                 </a>
             </Link>
         )}
