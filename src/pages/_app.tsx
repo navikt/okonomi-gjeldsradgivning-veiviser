eb6ffbcb082547d8154a628daf7403b0d80c7565
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-01-21 15:39:01 +0100

    Add amplitude (#62)
    * Legger til Amplitude og teller klikk på eksterne lenker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import { AppProps, NextWebVitalsMetric } from 'next/app';
+import { useEffect } from 'react';
 import '../index.less';
+import { initAmplitude } from '../utils/amplitude';
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
     console.log(metric);
 }
 
-const CustomApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const CustomApp = ({ Component, pageProps }: AppProps) => {
+    useEffect(() => {
+        initAmplitude();
+    }, []);
+
+    return <Component {...pageProps} />;
+};
 
 export default CustomApp;
