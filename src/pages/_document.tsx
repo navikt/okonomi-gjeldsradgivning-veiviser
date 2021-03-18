b70fb8238803e4f59fd14eb1ad6a945b05ec4c8f
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-03-18 11:26:20 +0100

    Legger på ssr for styled components ved første sidelasting (#107)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,8 @@
 import { Components, ENV, fetchDecoratorReact, Props } from '@navikt/nav-dekoratoren-moduler/ssr';
+import { NextPageContext } from 'next';
+import { RenderPage } from 'next/dist/next-server/lib/utils';
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 const decoratorEnv = process.env.DECORATOR_ENV as Exclude<ENV, 'localhost'>;
 
@@ -10,9 +13,9 @@ const decoratorParams: Props = {
 };
 class CustomDocument extends Document<{ decorator: Components }> {
     static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx);
+        const styledComponentsStylesheet = await renderServersideStyledComponentsStylesheet(ctx);
         const decorator = await fetchDecoratorReact(decoratorParams);
-        return { ...initialProps, decorator };
+        return { ...styledComponentsStylesheet, decorator };
     }
 
     render() {
@@ -31,6 +34,30 @@ class CustomDocument extends Document<{ decorator: Components }> {
                 </body>
             </Html>
         );
+    }
+}
+
+// https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
+async function renderServersideStyledComponentsStylesheet(ctx: NextPageContext & { renderPage: RenderPage }) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+            });
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+            ...initialProps,
+            styles: (
+                <>
+                    {initialProps.styles}
+                    {sheet.getStyleElement()}
+                </>
+            ),
+        };
+    } finally {
+        sheet.seal();
     }
 }
 
