4c8da1810034a6f2789be729b365a052ebc6ca03
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-06-04 12:51:06 +0200

    Justerer størrelse på overskrifter

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -9,7 +9,7 @@ export const Article = (props: { article: SanityArticle }) => (
     <article>
         <ArticlePanel>
             <PanelIcon imageUrl={props.article.iconUrl} />
-            <Title level={1} size="xl" spacing>
+            <Title level={1} size="2xl" spacing>
                 {props.article.title}
             </Title>
             <SanityBlockContent blocks={props.article.body} />
