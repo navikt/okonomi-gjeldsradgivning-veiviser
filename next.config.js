bc6f00ca0594982a8cea6da9623d217219829e47
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-07-08 09:48:29 +0200

    Dekorator uten less (#153)
    * Oppgraderer dekoratøren-moduler og fjerner resterende nav-frontend
    
    * Fjerner next-less plugin og config for å bygge med less

--- next.config.js
+++ next.config.js
@@ -1,37 +1,28 @@
-const withCss = require('@zeit/next-css');
-const withLess = require('@zeit/next-less');
 const packageJson = require('./package.json');
 const navFrontendModuler = [];
 Object.keys(packageJson.dependencies).forEach((key) => {
-    if (key.startsWith('nav-frontend-')) {
-        navFrontendModuler.push(key);
-    }
     if (key.startsWith('@navikt')) {
         navFrontendModuler.push(key);
     }
 });
 const withTranspileModules = require('next-transpile-modules')(navFrontendModuler);
-module.exports = withTranspileModules(
-    withCss(
-        withLess({
-            basePath: '/okonomi-og-gjeld',
-            target: 'server',
-            trailingSlash: false,
-            reactStrictMode: true,
+module.exports = withTranspileModules({
+    basePath: '/okonomi-og-gjeld',
+    target: 'server',
+    trailingSlash: false,
+    reactStrictMode: true,
 
-            eslint: {
-                build: true,
-            },
+    eslint: {
+        build: true,
+    },
 
-            async redirects() {
-                return [
-                    {
-                        source: '/artikkel/jeg-trenger-hjelp-av-en-okonomi-og-gjeldsradgiver-i-nav',
-                        destination: '/artikkel/fa-gjeldsradgivning-fra-nav',
-                        permanent: true,
-                    },
-                ];
+    async redirects() {
+        return [
+            {
+                source: '/artikkel/jeg-trenger-hjelp-av-en-okonomi-og-gjeldsradgiver-i-nav',
+                destination: '/artikkel/fa-gjeldsradgivning-fra-nav',
+                permanent: true,
             },
-        })
-    )
-);
+        ];
+    },
+});
