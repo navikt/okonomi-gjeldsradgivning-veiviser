46cb313e3762cc94bad6c6a27ad37ff81b516d15
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-08-30 13:42:44 +0200

    Oppgradere sanity (#171)
    * Oppgraderer sanity

--- src/utils/sanity-client.ts
+++ src/utils/sanity-client.ts
@@ -5,6 +5,6 @@ import sanityConfig from '../../sanity/sanity.json';
 export default sanityClient({
     projectId: sanityConfig.api.projectId,
     dataset: process.env.SANITY_DATASET,
-    apiVersion: '2021-03-25',
+    apiVersion: '2021-08-30',
     useCdn: true,
 });
