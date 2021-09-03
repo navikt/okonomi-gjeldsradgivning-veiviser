66a69842a922c19e2295cdc1f28dfc4e61a204bd
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-09-03 14:26:01 +0200

    Diverse oppgraderinger (#180)
    * Oppgraderer next
    
    * Oppgradere eslint og retter feil
    
    * Oppgraderer jest
    
    * Upgrade styled-components
    
    * Upgrade typescript
    
    * upgrade eslint-plugin-prettier
    
    * Upgrade babel
    
    * Upgrade dekoratør og html-react-parser
    
    * Upgrade amplitude
    
    * Upgrade vimeo
    
    * Oppgraderer @navikt/ds-*
    
    * Upgrade types
    
    * Oppgraderer husky og lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { BodyShort, Cell, ContentContainer, Grid, Panel, Title } from '@navikt/ds-react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
 import styled from 'styled-components/macro';
 
