b0488759a36f81fd2965ce065e76d96d20c62e76
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-09-13 08:59:50 +0200

    Riktig dataset

--- src/lib/sanity/config.ts
+++ src/lib/sanity/config.ts
@@ -2,7 +2,7 @@ import sanityConfig from '../../../sanity/sanity.json';
 
 export const config = {
     projectId: sanityConfig.api.projectId,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    dataset: process.env.SANITY_DATASET,
     apiVersion: '2021-08-30',
     useCdn: true,
     withCredentials: true,
