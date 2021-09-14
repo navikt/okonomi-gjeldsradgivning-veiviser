85376dc216ce2d28ad19082674511058d5061080
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-09-14 12:06:46 +0200

    Eksporterer dataset til frontend slik at usePreviewSubsciption kan bruke riktig dataset

--- src/utils/sanity-client.ts
+++ src/utils/sanity-client.ts
@@ -4,7 +4,7 @@ import sanityConfig from '../../sanity/sanity.json';
 
 export default sanityClient({
     projectId: sanityConfig.api.projectId,
-    dataset: process.env.SANITY_DATASET,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: '2021-08-30',
     useCdn: true,
 });
