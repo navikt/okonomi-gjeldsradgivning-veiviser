d1fe4e7bf9b80716574dabc78f26b2063e3e9a9b
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-10-26 12:18:01 +0200

    Legger til environment for ulike miljøer

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -10,6 +10,7 @@ Sentry.init({
     dsn: SENTRY_DSN || 'https://e1ebfd84266b4eecb8fb9a286fb06e7c@sentry.gc.nav.no/58',
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1.0,
+    environment: process.env.NEXT_PUBLIC_ENVIRONMENT ?? 'prod-gcp',
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
