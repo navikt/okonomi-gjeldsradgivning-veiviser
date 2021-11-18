b690891677b09078b925cd04a1ff9c3485058f9d
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-11-18 10:48:35 +0100

    Logger url-parameterene kommerFra og video til amplitude i forbindelse med kampenjevideo på sosiale medier (#206)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,29 @@
 import '@navikt/ds-css';
 
 import { AppProps, NextWebVitalsMetric } from 'next/app';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-import { initAmplitude } from '../utils/amplitude';
+import { initAmplitude, logAmplitudeEvent } from '../utils/amplitude';
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
     console.log(metric);
 }
 
 const CustomApp = ({ Component, pageProps }: AppProps) => {
+    const router = useRouter();
+    const kommerFra = router.query['kommerFra'];
+    const video = router.query['video'];
+
     useEffect(() => {
         initAmplitude();
     }, []);
+
+    useEffect(() => {
+        if (kommerFra && video) {
+            logAmplitudeEvent('sidevisning', { kommerFra, video });
+        }
+    }, [kommerFra, video]);
 
     return <Component {...pageProps} />;
 };
