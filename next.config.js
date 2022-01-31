d7359ad199df366677d67abbaba02959a08ebc71
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-01-31 13:26:24 +0100

    Bumper nextjs og sentry for å patche et par sårbarheter

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ const moduleExports = {
     },
 };
 
-if (process.env.ENABLE_SENTRY) {
+if (process.env.ENABLE_SENTRY === "true") {
     module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
 } else {
     module.exports = moduleExports;
